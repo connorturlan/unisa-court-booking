0a2b7e8c7cf91700bb8a07f1c003836197134545
Connor <connorturlan@gmail.com>
2024-03-02 14:43:52 +1030

    initial commit

---
+++ aws/src/bookings/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+const (
+	region string = "ap-southeast-2"
+)
+
+func getClient() *dynamodb.DynamoDB {
+	config := aws.NewConfig().WithRegion(region)
+
+	session, err := session.NewSession()
+	if err != nil {
+		return nil
+	}
+
+	return dynamodb.New(session, config)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// sourceIP := request.RequestContext.Identity.SourceIP
+
+	client := getClient()
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"uuid": {
+				S: aws.String("f208a62c770121a58fe0f25bf00ebec1fe933f3947287f0342d5eafca368e63e"),
+			},
+		},
+		TableName: aws.String("unisa-hashes"),
+	}
+
+	result, err := client.GetItem(input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: 500,
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("%s\n", result),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
